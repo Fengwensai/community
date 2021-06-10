4e20b9a6aff876e4cd26ccd946df991f542ab38e
fengwensai <fws153957@163.com>
2021-06-10 11:16:10 +0800

    add index and bootstrap

--- src/main/java/life/majiang/community/community/controller/IndedxController.java
+++ src/main/java/life/majiang/community/community/controller/IndedxController.java
@@ -6,13 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller     //Controller注解可简单理解为允许该类接收前台传递过来的参数
-public class HelloController {
+public class IndedxController {
 
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name = "name") String name, Model model){
-        model.addAttribute("name",name);
-        return "hello";
-    }
+    @GetMapping("/")
+    public String hello(){ return "index"; }
 
 
 }
